0d086f075e5aad08de8cbcab58d6276d271dbc2b
NickNatali <nicka.natali@calbaptist.edu>
2017-01-06 23:48:50 -0800

    Allows users to find locations on a mapview and add to a list

---
+++ memorable places/Memorable Places/PlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlacesViewController.h
+//  Memorable Places
+//
+//  Created by Nick Natali on 12/30/16.
+//  Copyright © 2016 Appfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesViewController : UITableViewController
+
+@end
